4161b37bbd0d010f209385d22924c842993bb676
YoungSeokHong <80624082+YoungSeokHong@users.noreply.github.com>
2021-08-27 21:16:07 +0900

    Test github action

--- app/src/main/java/com/example/listen_my_order/activities/ImportMenuActivity.java
+++ app/src/main/java/com/example/listen_my_order/activities/ImportMenuActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.listen_my_order.R;
-import com.example.listen_my_order.adapter.ImportMenuAdapter;
+import com.example.listen_my_order.adapters.ImportMenuAdapter;
 
 import java.util.ArrayList;
 
