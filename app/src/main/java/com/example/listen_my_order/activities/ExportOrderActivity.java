9a82c090ae0c90e4f13b4d93ab40e21471547521
HACore <lhe5254@gmail.com>
2021-09-01 00:17:59 +0900

    Add ExportOrderAdapter & Update ExportOrderActivity

--- app/src/main/java/com/example/listen_my_order/activities/ExportOrderActivity.java
+++ app/src/main/java/com/example/listen_my_order/activities/ExportOrderActivity.java
@@ -2,16 +2,34 @@ package com.example.listen_my_order.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.listen_my_order.R;
+import com.example.listen_my_order.adapters.ExportOrderAdapter;
 
 import java.util.ArrayList;
 
 public class ExportOrderActivity extends AppCompatActivity {
+    // Components
+    private ActionBar appbar;
+    private Button exportButton;
+    private TextView totalPriceView;
 
+    // For orderList
+    private RecyclerView orderListView;
+    private ExportOrderAdapter exportOrderAdapter;
     private ArrayList<MenuData> orderList = new ArrayList<>();
+
+    // Properties
+    private float priceSum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,45 @@ public class ExportOrderActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         orderList = (ArrayList<MenuData>) intent.getSerializableExtra("OrderList");
+
+        // Set ActionBar
+        appbar = getSupportActionBar();
+        appbar.setTitle("Order menu");
+        appbar.setDisplayHomeAsUpEnabled(true);
+
+        // Set components
+        exportButton = (Button) findViewById(R.id.btn_export_order);
+        totalPriceView = (TextView) findViewById(R.id.tv_total_price);
+        orderListView = (RecyclerView) findViewById(R.id.rv_order_list);
+        orderListView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Set adapter to recyclerView
+        exportOrderAdapter = new ExportOrderAdapter(orderList);
+        orderListView.setAdapter(exportOrderAdapter);
+
+        // Set total price view
+        for(MenuData order : orderList) {
+            priceSum += order.getPrice();
+        }
+        totalPriceView.setText("$ " + Float.toString(priceSum));
+
+        // setOnClickListener for exportButton
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Todo: 데이터 송출
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
